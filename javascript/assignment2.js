292dcc1fddefbefc06e0822a606fe4fd469fc825
FatimaNaeem4144 <fatimanaeem969@gmail.com>
2023-04-03 13:06:47 -0700

    assignment done

---
+++ javascript/assignment2.js
@@ -0,0 +1,41 @@
+        // Question No 1
+let username;
+
+        // Question No 2
+let myName = "Fatima Naeem";
+
+        // Question No 3
+let message = "Hello World!";
+alert(message);
+
+        // Question No 4
+let name = "Fatima";
+let age = "17 years old";
+let certified = "Certified Mobile Application Development"
+
+alert(name);
+alert(age);
+alert(certified);
+
+        // Question No 5
+let name1 = "PIZZA";
+alert(name1);
+alert(name1.slice(0, name1.length-1));
+alert(name1.slice(0, name1.length-2));
+alert(name1.slice(0, name1.length-3));
+alert(name1.slice(0, name1.length-4));
+
+        // Question No 6
+let email = "fatimanaeem969@gmail.com";
+alert("My Email Address is " + email)
+
+
+        // Question No 7
+let book = "A Smarter Way To Learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+        // Question No 8
+document.write("Yah! I can write HTML content through JavaScript");
+
+let symbol = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ";
+alert(symbol);
