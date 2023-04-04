7cafef706bf868b3897c627619b0043a9b3cd20b
FatimaNaeem4144 <fatimanaeem969@gmail.com>
2023-04-04 04:40:18 -0700

    assignment done

---
+++ assignment1.js
@@ -0,0 +1,58 @@
+// *******************CH 1**********************
+        // Question No 1
+alert("Hello User!  Welcome to the website");
+
+        // Question No 2
+alert("Error! Please enter a valid password.");
+
+        // Question No 3
+alert("Welcome to JS Land.. \nHappy Coding!");
+
+        // Question No 4
+alert("Welcome to JS Land..");
+alert("Happy Coding!");
+
+         // Question No 5
+console.log("Hello... I can run JS through my web browser's console")
+
+// *******************CH 2**********************
+         // Question No 1
+var userName;
+
+         // Question No 2
+let myName = "Fatima Naeem";
+
+         // Question No 3
+let message = "Hello World!"
+alert(message)
+
+         // Question No 4
+let name = "John Doe";
+let age = "17 Years old";
+let certificate = "Certified Mob App development";
+alert(name);
+alert(age);
+alert(certificate);
+
+         // Question No 5
+let name1 = "PIZZA";
+alert(name1);
+alert(name1.slice(0, name1.length-1));
+alert(name1.slice(0, name1.length-2));
+alert(name1.slice(0, name1.length-3));
+alert(name1.slice(0, name1.length-4));
+
+         // Question No 6
+let email = "fatimanaeem989@gmail.com";
+alert("My email address is " + email);
+
+         // Question No 7
+let book = "A Smarter Way to Learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+         // Question No 8
+document.write("Yah!I can write HTML content through JavaScript.");
+
+         // Question No 9
+let symbols = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(symbols);
