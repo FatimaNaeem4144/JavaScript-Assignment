b0d90eddfb87b1866362f0eb09abe7f884684b4f
FatimaNaeem4144 <fatimanaeem969@gmail.com>
2023-04-04 03:54:44 -0700

    assignment done

--- javascript/assignment1.js
+++ javascript/assignment1.js
@@ -14,3 +14,10 @@ alert("Happy Coding!");
          // Question No 5
 console.log("Hello... I can run JS through my web browser's console")
 
+
+
+// let userName = prompt("What is your name?");
+// let age = prompt("What is your age?");
+// let hobby = prompt("What is your hobby?");
+// alert("Thanks " + userName + "!");
+// document.write("Username is " + userName + "<hr>" + "Userage is " + age + "<hr>" + "hobby is" + hobby);
