0ebf7c962e7d032f1cb203a238a46fefbb412e54
FatimaNaeem4144 <fatimanaeem969@gmail.com>
2023-06-04 10:09:29 -0700

    added new file

---
+++ Assignment1/CH 14,15,16,17/app.js
@@ -0,0 +1,113 @@
+        // Question No 1
+// const studentNames = [];
+
+//         // Question No 2
+// const students = { 
+//     name: []
+//  };
+
+//         // Question No 3
+// const stringArray = ["This is a string"];
+
+//         // Question No 4
+// const numberArray = [1,2,3,4];
+
+//         // Question No 5
+// const booleanArray = [true];
+
+//         // Question No 6
+// const mixedArray = ["string", 5, true];
+
+//         // Question No 7
+// const qualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.", "PhD"];
+// document.write("<h4>Qualifications:</h4>");
+// document.write("<ol>");
+// document.write("<li>" + qualification[0]);
+// document.write("<li>" + qualification[1]);
+// document.write("<li>" + qualification[2]);
+// document.write("<li>" + qualification[3]);
+// document.write("<li>" + qualification[4]);
+// document.write("<li>" + qualification[5]);
+// document.write("<li>" + qualification[6]);
+// document.write("<li>" + qualification[7]);
+
+        // Question No 8
+// const studentNames = ["Michael", "John", "Tony"];
+// const score = [320, 230, 480];
+// const total = 500;
+// const percentage1 = (score[0]*100)/total;
+// const percentage2 = (score[1]*100)/total;
+// const percentage3 = (score[2]*100)/total;
+// document.write("Score of " + studentNames[0] + " is " + score[0] +". Percentage: "+ percentage1 + "%");
+// document.write("<br> Score of " + studentNames[1] + " is " + score[1] +". Percentage: "+ percentage2 + "%");
+// document.write("<br> Score of " + studentNames[2] + " is " + score[2] +". Percentage: "+ percentage3 + "%");
+
+        // Question No 9
+// let colors = ["pink","blue","purple","black","violet"];
+// document.write(colors + "<br>");
+// let addColor1 = prompt("What color you want to add to the beginning");
+// colors.unshift(addColor1);
+// document.write(colors + "<br>");
+// let addColor2 = prompt("What color you want to add to the end");
+// colors.push(addColor2);
+// document.write(colors + "<br>");
+// colors.unshift("orange","yellow");
+// document.write(colors + "<br>");
+// colors.splice(0,1);
+// document.write(colors + "<br>");
+// colors.splice(-1,1);
+// document.write(colors + "<br>");
+// let addColor3 = prompt("At which index you want to add a color");
+// let userColor1 = prompt("Color name you want to add");
+// colors.splice(addColor3,1,userColor1);
+// document.write(colors + "<br>");
+// let delIndexColor = prompt("At which index you want to delete a color");
+// let delColor = prompt("How many colors you want to delete?");
+// colors.splice(delIndexColor,delColor);
+// document.write(colors);
+
+        // Question No 10
+// const studentsScores = [320,230,480,120];
+// document.write("Scores of Students: " + studentsScores);
+// const orderedScores = studentsScores.sort();
+// document.write("<br>Ordered Scores of Students: " + orderedScores);
+
+        // Question No 11
+// const cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// document.write("<h4> Cities list: </h4>" + cities);
+// const selectedCities = cities.slice(2,4);
+// document.write("<h4> Selected cities list: </h4>" + selectedCities);
+
+        // Question No 12
+// var arr = ["This", "is","my", "cat"];
+// var joinedArray = arr.join(" ");
+// document.write("Array: <br>" + arr);
+// document.write("<br><br>String: <br>" + joinedArray);
+
+        // Question No 13
+// const devices = ["keyboard","mouse","printer","monitor"];
+// document.write("Devices: <br> "+ devices);
+// document.write("<br><br>Out: <br>" + devices[0]);
+// document.write("<br>Out: <br>" + devices[1]);
+// document.write("<br>Out: <br>" + devices[2]);
+// document.write("<br>Out: <br>" + devices[3]);
+
+        // Question No 14
+// const devices = ["keyboard","mouse","printer","monitor"];
+// document.write("Devices: <br> "+ devices);
+// document.write("<br><br>Out: <br>" + devices[3]);
+// document.write("<br>Out: <br>" + devices[2]);
+// document.write("<br>Out: <br>" + devices[1]);
+// document.write("<br>Out: <br>" + devices[0]);
+
+        // Question No 15
+// const phones = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
+// document.write("<select>");
+//   document.write('<option>' + phones[0] + '</option>');
+//   document.write('<option>' + phones[2] + '</option>');
+//   document.write('<option>' + phones[3] + '</option>');
+//   document.write('<option>' + phones[1] + '</option>');
+//   document.write('<option>' + phones[4] + '</option>');
+//   document.write('<option>' + phones[5] + '</option>');
+// document.write("</select");
+  
