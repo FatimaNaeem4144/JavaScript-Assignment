7b2a8d34f274dd45d72a512ca64fb36c1fe9066c
FatimaNaeem4144 <fatimanaeem969@gmail.com>
2023-06-07 17:06:29 -0700

    added new file

---
+++ Assignment1/CH 18,19,20/app.js
@@ -0,0 +1,107 @@
+        // Question No 1
+let array = [""];
+
+        // Question No 2
+// let matrix = [
+//         [0, 1, 2, 3],
+//         [1, 0, 1, 2],
+//         [2, 1, 0, 1]
+// ];
+// for(let i=0;i<matrix.length;i++){
+//         for (let j=0;j<matrix[i].length;j++){
+//                 document.write(matrix[i][j] + " ");
+//         }
+//         document.write("<br>");
+// }
+
+        // Question No 3
+// for(let i=1;i<=10;i++){
+//     document.write(i + "<br>");
+// }
+
+        // Question No 4
+// let tableNumber = prompt("Enter a number to show its multiplication table");
+// let tableLength = prompt("Enter length multiplication table");
+// document.write("Multiplication table of " + tableNumber);
+// document.write("<br> Length " + tableLength + "<br>");
+
+// for (var i=1; i<=tableLength ; i++){
+//     document.write("<br>" + tableNumber + " X " + i + " = " + (tableNumber*i));
+// }
+
+        // Question No 5
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (let i=0;i<5;i++){
+//     document.write(fruits[i] + "<br>");
+// }
+// for(let i=0;i<5;i++){
+// document.write("Element at index " + i + " is " + fruits[i] + "<br>");
+// }
+
+        // Question No 6
+// document.write("<h3> Counting: </h3> <br>")
+// for(i=1;i<=15;i++){
+//     document.write(i + ",");
+// }
+// document.write("<h3>Reverse Counting: </h3> <br>")
+// for(i=10;i>=1;i--){
+//     document.write(i + ",");
+// }
+// document.write("<h3>Even: </h3> <br>")
+// for(i=0;i<=20;i++){
+//     if(i%2==0){
+//     document.write(i + ",");}
+// }
+// document.write("<h3>Odd: </h3> <br>")
+// for(i=0;i<=20;i++){
+//     if(i%2!==0){
+//     document.write(i + ",");}
+// }
+// document.write("<h3>Series: </h3> <br>")
+// for(i=1;i<=20;i++){
+//     if(i%2==0){
+//     document.write(i + "k,");}
+// }
+
+        // Question No 7
+// let arr = ["cake","apple pie","cookie","chips","patties"];
+// let userInput = prompt("Welcome to ABC Bakery.What do you want to order sir/ma'am?");
+// let ans = false;
+// for(let i=0;i<arr.length;i++){
+//         if(userInput.toLowerCase()===arr[i]){
+//                 document.write(arr[i] + " is available at index " + i + " in our bakery");
+//                 ans = true;
+//                 break;
+//         }
+// }
+// if(!ans){
+//         document.write("We are sorry " + userInput + " is not available in our bakery");
+// }
+
+       // Question No 8
+// let array = [24,53,78,91,12];
+// document.write("Array items: " + array + "<br>");
+// let largest = array[0];
+// for(let i=1;i<array.length;i++){
+//         if(array[i] > largest){
+//                 largest = array[i];
+//         }
+// }
+// document.write("The largest number is " + largest)
+
+       // Question No 9
+// let array = [24,53,78,91,12];
+// document.write("Array items: " + array + "<br>");
+// let smallest = array[0];
+// for(let i=1;i<array.length;i++){
+//         if(array[i] < smallest){
+//                 smallest = array[i];
+//         }
+// }
+// document.write("The smallest number is " + smallest)
+
+
+       // Question No 10
+// for (i=1;i<=20;i++){
+//         document.write((5*i) + ",");
+// }
